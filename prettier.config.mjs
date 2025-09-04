56f7baca46174c751a146aa651404b7a4e5d5dc4
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-09-04 13:22:46 +0530

    chore: bumped the version of the chart

--- prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-	semi: true,
-	trailingComma: 'all',
-	singleQuote: true,
-	printWidth: 80,
-	tabWidth: 2,
-	useTabs: false,
-};
